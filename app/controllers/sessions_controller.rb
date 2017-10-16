21f903baf8dd10967d2343d32d9318a2383b94bb
Shaunna <skwiens@gmail.com>
2017-10-16 13:38:05 -0700

    login github feature and button

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,6 @@
 class SessionsController < ApplicationController
 
   def login_form
-
   end
 
   def login
@@ -14,6 +13,27 @@ class SessionsController < ApplicationController
     else
       flash[:error] = "No author found by the name #{ params[:username] } "
       redirect_to root_path
+    end
+  end
+
+  def create
+    @auth_hash = request.env['omniauth.auth']
+    ap @auth_hash
+
+    @user = User.find_by(uid: @auth_hash['uid'], provider: @auth_hash['provider'])
+
+    if @user
+      session[:user_id] = @user.id
+      flash[:success] = "Welcome #{@user.name}"
+    else
+      @user = User.new uid: @auth_hash['uid'], provider: @auth_hash['provider'], name: @auth_hash['info']['nickname'], email: @auth_hash['info']['email']
+
+      if @user.save
+        session[:user_id] = @user.id
+        flash[:success] = "Welcome #{@user.name}"
+      else
+        flash[:error] = "Unable to save user!"
+      end
     end
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171004211231) do
+ActiveRecord::Schema.define(version: 20171016171859) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,15 @@ ActiveRecord::Schema.define(version: 20171004211231) do
 
   create_table "genres", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.integer "uid", null: false
+    t.string "provider", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
